5a1baf914e8689a6b726c4b4b4cbcdc6d6ec8b9b
dali <dalikim@parti.xyz>
2016-09-30 01:30:06 +0900

    추모글 폼을 검증합니다

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,8 @@
 //= require unobtrusive_flash
 //= require unobtrusive_flash_bootstrap
 //= require jquery.infinitescroll
-//
+//= require jquery.validate
+
 UnobtrusiveFlash.flashOptions['timeout'] = 3000;
 
 $(document).on('ready', function(e) {
@@ -20,10 +21,17 @@ $(document).on('ready', function(e) {
     nextSelector: "ul.pagination a[rel=next]",
     itemSelector: "section#comments-section .comment"
   });
-//     navSelector: "nav.pagination" # selector for the paged navigation (it will be hidden)
-//     nextSelector: "nav.pagination a[rel=next]" # selector for the NEXT link (to page 2)
-//     itemSelector: "#posts tr.post" # selector for all items you'll retrieve
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+  $('#new_comment').validate({
+    ignore: ':hidden',
+    errorPlacement: function(error, element) {
+      return true;
+    },
+    highlight: function(element, errorClass, validClass) {
+      $(element).closest('.form-group').addClass('field_with_errors');
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).closest('.form-group').removeClass('field_with_errors');
+    }
+  });
 });
