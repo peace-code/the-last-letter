742886fe257c4c72f16c1f39a3afa6b9554efa0b
rest515 <rest515@gmail.com>
2016-09-30 00:33:25 +0900

    publishing header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,12 @@
 //= require unobtrusive_flash_bootstrap
 
 UnobtrusiveFlash.flashOptions['timeout'] = 3000;
+
+$(document).on('ready', function(e) {
+  $('.btn-show-form').on('click', function(e) {
+    $('#comment-btn-show-form-box').hide();
+    $('#comment-form-box').show();
+    $('#comment-form-box #comment_nickname').focus();
+    return false;
+  });
+});
