0e6a375d2007cd039655a3ade5b7cf5a830540d6
dali <dalikim@parti.xyz>
2016-09-30 01:09:10 +0900

    무한스크롤이 됩니다

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,8 @@
 //= require bootstrap
 //= require unobtrusive_flash
 //= require unobtrusive_flash_bootstrap
-
+//= require jquery.infinitescroll
+//
 UnobtrusiveFlash.flashOptions['timeout'] = 3000;
 
 $(document).on('ready', function(e) {
@@ -13,4 +14,16 @@ $(document).on('ready', function(e) {
     $('#comment-form-box #comment_nickname').focus();
     return false;
   });
+
+  $("section#comments-section .comments-list .page").infinitescroll({
+    navSelector: "ul.pagination",
+    nextSelector: "ul.pagination a[rel=next]",
+    itemSelector: "section#comments-section .comment"
+  });
+//     navSelector: "nav.pagination" # selector for the paged navigation (it will be hidden)
+//     nextSelector: "nav.pagination a[rel=next]" # selector for the NEXT link (to page 2)
+//     itemSelector: "#posts tr.post" # selector for all items you'll retrieve
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
 });
