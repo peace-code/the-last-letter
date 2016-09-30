71066fceab6a83c369d0d0197f0eabbb58c52cd2
rest515 <rest515@gmail.com>
2016-09-30 13:35:04 +0900

    신고되어 스크리닝하는 글 기준을 줄입니다

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   has_many :reports, dependent: :destroy
   default_scope { order("id DESC") }
 
-  REPORT_LIMIT = 100
+  REPORT_LIMIT = 15
 
   def screened?
     reports_count > REPORT_LIMIT
